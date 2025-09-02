185c0fb07ca36e931272db8898c3e27c9c2c2d60
Dawit Eshete <164386491+Deesprings@users.noreply.github.com>
2025-09-01 19:01:10 -0700

    Trouble Shooting Compiler

--- Lab-01-starter/firmware/Lab-01-starter.X/nbproject/Makefile-local-sam_e51_cnano.mk
+++ Lab-01-starter/firmware/Lab-01-starter.X/nbproject/Makefile-local-sam_e51_cnano.mk
@@ -21,18 +21,17 @@ PATH:=D:/mplab_platform/platform/../mplab_ide/modules/../../bin/:$(PATH)
 # Path to java used to run MPLAB X when this makefile was created
 MP_JAVA_PATH="D:\sys\java\zulu8.64.0.19-ca-fx-jre8.0.345-win_x64/bin/"
 OS_CURRENT="$(shell uname -s)"
-MP_CC="C:\Program Files\Microchip\xc32\v4.60\bin\xc32-gcc.exe"
-MP_CPPC="C:\Program Files\Microchip\xc32\v4.60\bin\xc32-g++.exe"
+MP_CC="D:\bin\xc32-gcc.exe"
+MP_CPPC="D:\bin\xc32-g++.exe"
 # MP_BC is not defined
-MP_AS="C:\Program Files\Microchip\xc32\v4.60\bin\xc32-as.exe"
-MP_LD="C:\Program Files\Microchip\xc32\v4.60\bin\xc32-ld.exe"
-MP_AR="C:\Program Files\Microchip\xc32\v4.60\bin\xc32-ar.exe"
+MP_AS="D:\bin\xc32-as.exe"
+MP_LD="D:\bin\xc32-ld.exe"
+MP_AR="D:\bin\xc32-ar.exe"
 DEP_GEN=${MP_JAVA_PATH}java -jar "D:/mplab_platform/platform/../mplab_ide/modules/../../bin/extractobjectdependencies.jar"
-MP_CC_DIR="C:\Program Files\Microchip\xc32\v4.60\bin"
-MP_CPPC_DIR="C:\Program Files\Microchip\xc32\v4.60\bin"
+MP_CC_DIR="D:\bin"
+MP_CPPC_DIR="D:\bin"
 # MP_BC_DIR is not defined
-MP_AS_DIR="C:\Program Files\Microchip\xc32\v4.60\bin"
-MP_LD_DIR="C:\Program Files\Microchip\xc32\v4.60\bin"
-MP_AR_DIR="C:\Program Files\Microchip\xc32\v4.60\bin"
+MP_AS_DIR="D:\bin"
+MP_LD_DIR="D:\bin"
+MP_AR_DIR="D:\bin"
 DFP_DIR=C:/Users/dawit/.mchp_packs/Microchip/SAME51_DFP/3.8.253
-CMSIS_DIR=
